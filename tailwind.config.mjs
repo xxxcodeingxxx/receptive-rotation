25e13944bc296cd92fea7b1af6ebff7c3d4a331a
John Downs <downspropertym@gmail.com>
2023-12-06 15:31:25 -0500

    Add new colors to theme configuration,factor index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "my-purple": "#9924F5",
+        "my-yellow": "#DEF037",
+        "muted-purple": "#7D579B",
+        baige: "#A7B054",
+        "dark-baige": "#6D7052",
+      },
+    },
+  },
+  plugins: [],
+};

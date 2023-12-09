03262929b0c0cde11c3ffec8aa8bd7f362eaf434
xxxcodeingxxx <downspropertym@gmail.com>
2023-12-08 21:12:16 -0500

    Add jQuery and its types as dependencies

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -40,3 +40,46 @@ document.getElementById('departDate').innerHTML = futureDay.toLocaleDateString(
 setTimeout(function () {
     location.reload()
 }, 60000)
+
+// Change colors based on odd or even day
+const bgColor = $('#bgColor')
+const sep = $('#seperator')
+const h3 = $('#h3')
+const depart = $('#departDate')
+const main = $('#mainBg')
+
+if (now.getDate() % 2 === 0) {
+    bgColor.removeClass('bg-my-yellow/75 text-white')
+    bgColor.addClass('bg-my-purple/75 text-my-yellow')
+    sep.removeClass('border-my-purple opacity-25')
+    sep.addClass('border-my-yellow opacity-100')
+    h3.removeClass('text-my-purple/50')
+    h3.addClass('text-my-yellow')
+    depart.removeClass('text-my-purple/50')
+    depart.addClass('text-my-yellow')
+
+    $('#div_1, #div_2, #div_3').each(function () {
+        $(this).removeClass('bg-my-purple/50')
+        $(this).addClass('bg-white/25')
+    })
+
+    main.css('background-image', 'url("/assets/caribbeanIsland.jpg")')
+}
+
+let day = now.getDay()
+
+if (day === 6 || day === 7) {
+    main.css('background-image', 'url("/assets/cruiseShip.jpg")')
+    bgColor.addClass('bg-sky-700/75')
+    sep.removeClass('border-my-purple opacity-25')
+    sep.addClass('border-white/75 opacity-100')
+    h3.removeClass('text-my-purple/50')
+    h3.addClass('text-white/75')
+    depart.removeClass('text-my-purple/50')
+    depart.addClass('text-white/75')
+
+    $('#div_1, #div_2, #div_3').each(function () {
+        $(this).removeClass('bg-my-purple/50')
+        $(this).addClass('bg-white/25')
+    })
+}
